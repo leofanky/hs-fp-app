ce4a3c8f3a6d8890328bf92b8c403b478165b552
leofanky <fillow99@gmail.com>
2017-11-02 13:22:17 +0100

    finale ?

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,27 +1,35 @@
+from json import loads, dumps
 def encrypt(This_key_is_awesome, message):  #better way of doing it cause it doesn't repeat
     encrypted_message = []
-    for i, c in enumarate(message): #assaning a number for every character
-        key_c = ord(key[i % len(This_key_is_awesome)])   #ord() string representing one Unicode character
+    for i, c in enumerate(message): #assaning a number for every character
+        key_c = ord(This_key_is_awesome[i % len(This_key_is_awesome)])   #ord() string representing one Unicode character
         message_c = ord (c)
         encrypted_message.append(chr((message_c + key_c) %127))
          #we are adding the message and the encryption key converted in Unicode character. The % is used to keep the resulting chr in the 7-bit ascii range
          #Think about % here like the clock: when it's x hours later than y 'o clock, it's (x+y) % 12 'o clock.) for afte ->
          #chr() a character (a string) whose Unicode code point is the integer i and after adding all of this to the list
-    return ''.join(encrypted_message)
+    return dumps(''.join(encrypted_message))
 
 
 def decrypt(This_key_is_awesome,encrypted_message):
+    encrypted_message = loads(encrypted_message)
     message = []
-    for i, c in enumerated(encrypted_message):
-         key_c = ord(key[i % len(This_key_is_awesome)])
+    for i, c in enumerate(encrypted_message):
+         key_c = ord(This_key_is_awesome[i % len(This_key_is_awesome)])
          encoded_c = ord(c)
-         message.append(chr((message_c - key_c) %127)) #we are reverse engeneering the encrypted message
+         message.append(chr((encoded_c - key_c) %127)) #we are reverse engeneering the encrypted message
     return''.join(message)
 
 
 def process_user_query(query_string):
-    This_key_is_awesome = 'This_is_my_awsome_secret_key'
-    message = query_string
-    encrypted = encrypt(This_key_is_awesome, message)
-    decrypted = decrypt(This_key_is_awesome, encrypted_message)
-    return {'Message:': repr(message), 'Key:': repr(This_key_is_awesome),'Encrypted:': repr(encrypted_message),'Decrypted:': repr(encrypted_message)}
+    q = query_string.splitlines()
+    if q[0] == 'encrypt':
+        return encrypt(q[1],q[2])
+    else:
+        return decrypt(q[1],q[2])
+
+
+
+#comand encrypt or  decrypt
+#key
+#message
